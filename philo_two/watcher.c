adc1ffa040a6e4caf2ecd8ae6979bf24227adb84
kh42z <tterrail@student.42.fr>
2020-10-15 12:26:26 +0200

    fix 2

--- philo_two/watcher.c
+++ philo_two/watcher.c
@@ -14,37 +14,34 @@
 
 static void	announce_death(t_philo *this)
 {
-	sem_wait(this->args.end->tid);
 	this->args.end->is_over = 1;
 	print_unprotected(this, "died\n");
-	sem_post(this->args.end->tid);
 }
 
 void		*is_he_dead(void *philo)
 {
 	t_philo	*this;
-	int		over;
 
 	this = (t_philo*)philo;
 	while (1)
 	{
 		sem_wait(this->eating);
 		sem_wait(this->args.end->tid);
-		over = this->args.end->is_over;
-		sem_post(this->args.end->tid);
-		if (over == 1 || this->args.nb_of_must_eat == 0)
-			break ;
 		if (is_dead(this) == 1)
 		{
-			if (over == 0)
+			if (this->args.end->is_over == 1 || this->args.nb_of_must_eat == 0)
+				break ;
+			if (this->args.end->is_over == 0)
 			{
 				announce_death(this);
 				break ;
 			}
 		}
+		sem_post(this->args.end->tid);
 		sem_post(this->eating);
 		usleep(1);
 	}
+	sem_post(this->args.end->tid);
 	sem_post(this->eating);
 	return (NULL);
 }
